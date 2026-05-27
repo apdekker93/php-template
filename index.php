32a3b51ece466ed97900dcbef8635459a60c95a2
apdekker93 <apdekker93@gmail.com>
2026-05-27 07:41:34 +0000

    Voeg een index-pagina toe.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP & SQL CodeSpace</title>
+    <meta type="charset" value="utf-8">
+</head>
+<body>
+    <h1>Welkom in de PHP & SQL CodeSpace!</h1>
+    <p>
+        Om het juiste bestand te openen, type je het pad naar dat bestand achter de URL die eindigt op .github.dev. Bijvoorbeeld: /oefenen/onderwerp-2/score.php.
+    </p>
+</body>
+</html>

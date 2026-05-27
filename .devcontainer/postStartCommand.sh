376bfa920c1ae80fbe79064bf2d95bc9f6da5aba
apdekker93 <apdekker93@gmail.com>
2026-05-27 15:23:08 +0200

    Configure PHP web server in postStartCommand.sh
    Start PHP web server with router.php for 404 handling.

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-// place commands here
+# Start de PHP webserver met router.php zodat ontbrekende bestanden
+# een correcte 404-melding geven in plaats van doorsturen naar index.php.
+php -S localhost:8080 router.php

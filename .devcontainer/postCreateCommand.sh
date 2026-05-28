c147d5901510032e67761fd3aa2e32b5346c2201
Adriaan Dekker <apdekker93@gmail.com>
2026-05-28 13:41:25 +0200

    Add docker feature to php

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,12 +6,13 @@ git config merge.ours.driver true
 git config --global merge.conflictStyle diff3
 
 # PHP foutmeldingen tonen in de browser (handig tijdens ontwikkeling)
-PHP_INI_DIR=$(php --ini | grep "Scan for" | awk -F': ' '{print $2}')
+PHP_INI_DIR=$(php --ini | grep "Scan for" | awk -F': ' '{print $2}' | tr -d '"')
 echo "display_errors = On
 display_startup_errors = On
 error_reporting = E_ALL" | sudo tee "$PHP_INI_DIR/99-debug.ini" > /dev/null
 
 # MySQL client en PHP mysqli-extensie installeren
+sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get update -qq
 sudo apt-get install -y -qq default-mysql-client php-mysql
 
